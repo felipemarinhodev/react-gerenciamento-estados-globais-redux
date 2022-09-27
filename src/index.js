028ebaac8cd632361edb497c307f5c9d0f506504
felipemarinhodev <felipmarinho@gmail.com>
2022-09-27 07:34:56 -0700

    a1.5 - add Navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from './components/Navbar';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Hello React!</div>
+    <Navbar />
   </React.StrictMode>
 );
