d3eebe5f55a1a03819986d410655fa5d4aa9c90c
felipemarinhodev <felipmarinho@gmail.com>
2022-09-27 08:28:15 -0700

    a1.8 - add elements in the component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,40 @@
 import styles from './Navbar.module.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import classNames from 'classnames';
+import {
+	RiShoppingCart2Line,
+	RiShoppingCartFill
+} from 'react-icons/ri'
 
 export default function Navbar() {
 
-	return <nav className={styles.nav}>
-		<Logo className={styles.logo} />
-	</nav>
+	const iconsProps = {
+		color: 'white',
+		size: 24
+	}
+
+	return (
+		<nav className={styles.nav}>
+			<Logo className={styles.logo} />
+			<div className={styles.links}>
+				<div>
+					<a href="/" className={classNames(styles.link, {
+						[styles.selected]: window.location.pathname === '/'
+					})}>Página inicial</a>
+				</div>
+
+			</div>
+			<div className={styles.busca}></div>
+			<div className={styles.icones}>
+				<a href='/carrinho'>
+					{window.location.pathname === '/carrinho'
+						?
+						<RiShoppingCartFill {...iconsProps} />
+						:
+						<RiShoppingCart2Line {...iconsProps} />
+					}
+				</a>
+			</div>
+		</nav>
+	)
 }
