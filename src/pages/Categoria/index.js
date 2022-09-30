7c6cafc1b4616cff32fb9ae289ba1db0ec9074f9
felipemarinhodev <felipmarinho@gmail.com>
2022-09-30 07:55:42 -0700

    4.2 - show items of a category

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -3,13 +3,15 @@ import Header from 'components/Header';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import styles from './Categoria.module.scss'
+
 export default function Categoria() {
 	const { nomeCategoria } = useParams()
-	const categoria = useSelector(
-		state => state.categorias.find(
-			categoria => categoria.id === nomeCategoria
-		)
-	)
+	const { categoria, itens } = useSelector(state => ({
+		categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+		itens: state.itens.filter(item => item.categoria === nomeCategoria)
+	}));
+
 	return (
 		<div>
 			<Header
@@ -17,6 +19,11 @@ export default function Categoria() {
 				descricao={categoria.descricao}
 				imagem={categoria.header}
 			/>
+			<div className={styles.itens}>
+				{itens?.map(item => (
+					<div key={item.id}>{item.titulo}</div>
+				))}
+			</div>
 		</div>
 	)
 }
