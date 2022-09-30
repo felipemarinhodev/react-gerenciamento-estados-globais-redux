7b0bdc5488d5c6c3d0b5e91581af1a9e0c197f5c
felipemarinhodev <felipmarinho@gmail.com>
2022-09-30 08:55:22 -0700

    a5.2 - refactor to use the Link component of the react-router-dom

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,34 @@
-import styles from './Navbar.module.scss';
-import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
 import {
 	RiShoppingCart2Line,
 	RiShoppingCartFill
-} from 'react-icons/ri'
+} from 'react-icons/ri';
+
+import styles from './Navbar.module.scss';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
 import Busca from 'components/Busca';
 
-export default function Navbar() {
+const iconsProps = {
+	color: 'white',
+	size: 24
+}
 
-	const iconsProps = {
-		color: 'white',
-		size: 24
-	}
+export default function Navbar() {
+	const location = useLocation()
+	const navigate = useNavigate()
 
 	return (
 		<nav className={styles.nav}>
-			<Logo className={styles.logo} />
+			<Logo className={styles.logo} onClick={() => navigate('/')} />
 			<div className={styles.links}>
 				<div>
-					<a href="/" className={classNames(styles.link, {
-						[styles.selected]: window.location.pathname === '/'
-					})}>Página inicial</a>
+					<Link to="/" className={classNames(styles.link, {
+						[styles.selected]: location.pathname === '/'
+					})}
+					>
+						Página inicial
+					</Link>
 				</div>
 
 			</div>
@@ -29,14 +36,14 @@ export default function Navbar() {
 				<Busca />
 			</div>
 			<div className={styles.icones}>
-				<a href='/carrinho'>
-					{window.location.pathname === '/carrinho'
+				<Link to='/carrinho'>
+					{location.pathname === '/carrinho'
 						?
 						<RiShoppingCartFill {...iconsProps} />
 						:
 						<RiShoppingCart2Line {...iconsProps} />
 					}
-				</a>
+				</Link>
 			</div>
 		</nav>
 	)
