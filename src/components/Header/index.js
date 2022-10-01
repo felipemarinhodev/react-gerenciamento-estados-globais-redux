d8e1a375ddad9899f2c8a11b7c2fa112fd1094dd
felipemarinhodev <felipmarinho@gmail.com>
2022-09-30 20:58:29 -0700

    a5.6 - alter component if there is an image or not

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,30 @@
 import styles from './Header.module.scss';
+import TituloComImagem from './TituloComImagem';
+import TituloSemImagem from './TituloSemImagem';
 
-export default function Header({ titulo, descricao, className = '', imagem }) {
+export default function Header({
+	titulo,
+	descricao,
+	className = '',
+	imagem
+}) {
 	return (
-		<header className={`${styles.header} ${className}`}>
-			<div className={styles['header-texto']}>
-				<h1>{titulo}</h1>
-				<h2>{descricao}</h2>
-			</div>
-			<div className={styles['header-image']}>
-				<img
-					alt={titulo}
-					src={imagem}
+		<header className={styles.header}>
+			{titulo && imagem &&
+				<TituloComImagem
+					titulo={titulo}
+					descricao={descricao}
+					imagem={imagem}
+					className={className}
 				/>
-			</div>
+			}
+			{titulo && !imagem &&
+				<TituloSemImagem
+					titulo={titulo}
+					descricao={descricao}
+					className={className}
+				/>
+			}
 		</header>
 	)
 }
